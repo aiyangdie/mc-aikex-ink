a9fdbfabc51d0839c538b5eddafb100c4440e4ed
艾阳 aiyangdie <aike1015@qq.com>
2026-09-23 05:15:37 +0800

    fix: make creature movement obstacle aware

--- js/structures.js
+++ js/structures.js
@@ -1,8 +1,8 @@
 /**
  * 管理结构预设：地狱门 / 小木屋 / 末地平台
  */
-import { BlockType } from './voxel.js?v=playerstyle7';
-import { spawnReturnPortal } from './portals.js?v=playerstyle7';
+import { BlockType } from './voxel.js?v=animalfix8';
+import { spawnReturnPortal } from './portals.js?v=animalfix8';
 
 export function buildStructure(world, id, cx, cy, cz) {
   const x = Math.floor(cx);
