6284cd617126abd2076528de66269387594c0a17
艾阳 aiyangdie <aike1015@qq.com>
2026-09-23 05:03:06 +0800

    feat: upgrade multiplayer player avatars

--- js/physics.js
+++ js/physics.js
@@ -5,7 +5,7 @@
  * - 随机安全复活点
  */
 import * as THREE from 'three';
-import { isSolid, Dim } from './voxel.js?v=playerstats6';
+import { isSolid, Dim } from './voxel.js?v=playerstyle7';
 
 const BULLET_SPEED = 95;
 const BULLET_GRAVITY = -18;
