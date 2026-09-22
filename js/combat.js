aad9f37c400447c97b996a178b44f29b67ffe596
艾阳 aiyangdie <aike1015@qq.com>
2026-09-23 05:15:23 +0800

    fix: make creature movement obstacle aware

--- js/combat.js
+++ js/combat.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
-import { Mage } from './mage.js?v=playerstyle7';
-import { isSolid } from './voxel.js?v=playerstyle7';
-import { CombatPhysics, pickRandomSpawn } from './physics.js?v=playerstyle7';
+import { Mage } from './mage.js?v=animalfix8';
+import { isSolid } from './voxel.js?v=animalfix8';
+import { CombatPhysics, pickRandomSpawn } from './physics.js?v=animalfix8';
 
 /**
  * AK：联机打玩家；单机/联机本地弹道可打动物（PvE）
