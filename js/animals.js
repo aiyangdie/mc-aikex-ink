223ed85f05d1db5b0ec9b18ac2072f6057c1990e
艾阳 aiyangdie <aike1015@qq.com>
2026-09-23 04:56:21 +0800

    feat: add defeat stats and animated player avatars

--- js/animals.js
+++ js/animals.js
@@ -3,9 +3,9 @@
  * 地狱：敌对侦察机
  */
 import * as THREE from 'three';
-import { BlockType, isSolid, Dim } from './voxel.js?v=mistboss5';
-import { ItemType } from './items.js?v=mistboss5';
-import { buildMobDrops } from './loot.js?v=mistboss5';
+import { BlockType, isSolid, Dim } from './voxel.js?v=playerstats6';
+import { ItemType } from './items.js?v=playerstats6';
+import { buildMobDrops } from './loot.js?v=playerstats6';
 
 const SPAWN_RADIUS = 28;
 const MIN_SPAWN_DIST = 4;
