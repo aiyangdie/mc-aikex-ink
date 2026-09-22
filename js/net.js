033c87cdaeae2e20b4590409a3ea5019906808c1
艾阳 aiyangdie <aike1015@qq.com>
2026-09-23 05:22:43 +0800

    fix: align creature models with terrain

--- js/net.js
+++ js/net.js
@@ -1,7 +1,7 @@
 /**
  * 联机客户端：房间码建房/加入，同步方块与玩家位置
  */
-import { apiUrl, wsUrl } from './config.js?v=animalfix8';
+import { apiUrl, wsUrl } from './config.js?v=groundfix9';
 
 export class NetClient {
   constructor() {
