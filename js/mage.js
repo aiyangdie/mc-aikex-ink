02adf384513c1e153550855d677cb73bdc566bc2
艾阳 aiyangdie <aike1015@qq.com>
2026-09-23 05:15:28 +0800

    fix: make creature movement obstacle aware

--- js/mage.js
+++ js/mage.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { isSolid } from './voxel.js?v=playerstyle7';
+import { isSolid } from './voxel.js?v=animalfix8';
 
 const BLINK_RANGE = 7.5;
 const BLINK_CD = 3200;
