f5e7c7338028f098acf1aa4189a5d80b93e2ee66
艾阳 aiyangdie <aike1015@qq.com>
2026-09-23 05:22:46 +0800

    fix: align creature models with terrain

--- js/physics.js
+++ js/physics.js
@@ -5,7 +5,7 @@
  * - 随机安全复活点
  */
 import * as THREE from 'three';
-import { isSolid, Dim } from './voxel.js?v=animalfix8';
+import { isSolid, Dim } from './voxel.js?v=groundfix9';
 
 const BULLET_SPEED = 95;
 const BULLET_GRAVITY = -18;
