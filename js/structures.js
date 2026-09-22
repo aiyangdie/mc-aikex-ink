b41101ecfe5e5d6f8ab12a10d0d2e49dcb4fc8c3
艾阳 aiyangdie <aike1015@qq.com>
2026-09-23 05:03:11 +0800

    feat: upgrade multiplayer player avatars

--- js/structures.js
+++ js/structures.js
@@ -1,8 +1,8 @@
 /**
  * 管理结构预设：地狱门 / 小木屋 / 末地平台
  */
-import { BlockType } from './voxel.js?v=playerstats6';
-import { spawnReturnPortal } from './portals.js?v=playerstats6';
+import { BlockType } from './voxel.js?v=playerstyle7';
+import { spawnReturnPortal } from './portals.js?v=playerstyle7';
 
 export function buildStructure(world, id, cx, cy, cz) {
   const x = Math.floor(cx);
